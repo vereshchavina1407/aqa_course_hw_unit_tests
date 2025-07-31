d7b87dde5de08e75b83a84ca34907595e6b39f76
vereshchavina1407 <105720997+vereshchavina1407@users.noreply.github.com>
2025-07-31 22:10:52 +0300

    Update task-2.js

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,9 +4,3 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
-let n = +prompt();
-let strN = String(n);
-strN_2 = strN+strN
-strN_3 = strN+strN+strN
-let result = parseInt(strN) + parseInt(strN_2) + parseInt(strN_3)
-console.log(result)
